eccf135d0834c61967a6a851b3346ec4bcc9a8c6
Malashykhina <no_parsley@i.ua>
2018-01-02 04:18:16 +0200

    movement and falling of figures

---
+++ Tetris/Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	// Groups
+	public AudioClip click;
+	public AudioClip drop;
+	public GameObject[] groups;
+	private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+		// Spawn initial Group
+		spawnNext();
+		audioSource=GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	public void spawnNext() {
+		// Random Index
+
+		int i = Random.Range(0, groups.Length);
+
+		// Spawn Group at current Position
+		Instantiate(groups[i],
+			transform.position,
+			Quaternion.identity);
+	}
+
+	public void playClickAudio(){
+		audioSource.PlayOneShot(click);
+	}
+	public void playDropAudio(){
+		audioSource.PlayOneShot(drop);
+	}
+}
